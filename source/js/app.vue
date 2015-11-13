ae5d16359bb82d0375a8dbe1b060f423a9857e8a
junichiro_kazama <junichiro_kazama@xon.jp>
2015-11-13 17:59:44 +0900

    vue-routerでdiv警告が出ている問題に対応

--- source/js/app.vue
+++ source/js/app.vue
@@ -1,30 +1,31 @@
 <style lang="sass"></style>
 
 <template lang="jade">
-  nav.navbar.navbar-default.navbar-static-top.l-nav-header(v-if="logined")
-    .navbar-header
-      a.navbar-brand(href="/") App
-    ul.nav.navbar-nav
-      li: a(v-link="{path: '/top'}") "取扱い商品名 (TOP)" 
-      li: a(v-link="{path: '/trade'}") 取引情報
-      li: a(v-link="{path: '/asset'}") 口座資産
-    ul.nav.navbar-nav.navbar-right
-      li.dropdown
-        a.dropdown-toggle(href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false")
-          | ○○ 様 
-          span.caret
-        ul.dropdown-menu(role="menu")
-          li: a(href="#")
-            i.fa.fa-fw.fa-user
-            |  アカウント情報
-          li.divider
-          li: a(href="#" @click.prevent="logout")
-            i.fa.fa-fw.fa-sign-out
-            |  ログアウト
-  nav.navbar.navbar-default.navbar-static-top(v-if="!logined")
-    .navbar-header
-      .navbar-brand App
-  router-view
+  .l-container-main
+    nav.navbar.navbar-default.navbar-static-top.l-nav-header(v-if="logined")
+      .navbar-header
+        a.navbar-brand(href="/") App
+      ul.nav.navbar-nav
+        li: a(v-link="{path: '/top'}") "取扱い商品名 (TOP)" 
+        li: a(v-link="{path: '/trade'}") 取引情報
+        li: a(v-link="{path: '/asset'}") 口座資産
+      ul.nav.navbar-nav.navbar-right
+        li.dropdown
+          a.dropdown-toggle(href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false")
+            | ○○ 様 
+            span.caret
+          ul.dropdown-menu(role="menu")
+            li: a(href="#")
+              i.fa.fa-fw.fa-user
+              |  アカウント情報
+            li.divider
+            li: a(href="#" @click.prevent="logout")
+              i.fa.fa-fw.fa-sign-out
+              |  ログアウト
+    nav.navbar.navbar-default.navbar-static-top(v-if="!logined")
+      .navbar-header
+        .navbar-brand App
+    router-view
 </template>
 
 <script lang="babel">
